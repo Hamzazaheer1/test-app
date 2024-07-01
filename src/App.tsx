c68e490734b19836845aaf43cd659e5c39e732e3
Hamza Zaheer <mhamzazaheer1@gmail.com>
2024-07-01 14:36:22 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { useSDK } from "@metamask/sdk-react";
+import "./App.css";
+
+function App() {
+  const [account, setAccount] = useState<string>();
+  const { sdk, connected, connecting, provider, chainId } = useSDK();
+
+  const connect = async () => {
+    try {
+      const accounts = await sdk?.connect();
+      setAccount(accounts?.[0]);
+    } catch (err) {
+      console.warn("failed to connect..", err);
+    }
+  };
+
+  console.log("hamza");
+
+  return (
+    <div className="App">
+      <button style={{ padding: 10, margin: 10 }} onClick={connect}>
+        Connect
+      </button>
+      {connected && (
+        <div>
+          <>
+            {chainId && "Connected chain: ${chainId}"}
+            <p></p>
+            {account && "Connected account: ${account}"}
+          </>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { MetaMaskProvider } from "@metamask/sdk-react";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  // <React.StrictMode>
+  <MetaMaskProvider
+    debug={false}
+    sdkOptions={{
+      checkInstallationImmediately: false,
+      dappMetadata: {
+        name: "Example React Dapp",
+        url: window.location.href,
+      },
+      // infuraAPIKey: process.env.INFURA_API_KEY,
+      // Other options
+    }}
+  >
+    <App />
+  </MetaMaskProvider>
+  // </React.StrictMode>
+);
