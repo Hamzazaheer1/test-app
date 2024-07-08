05c8af7dc2da48d700fa5b52f472f024e54a2458
Hamza Zaheer <mhamzazaheer1@gmail.com>
2024-07-08 21:08:09 +0500

    vanilla csss

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ const App = () => {
       <input
         type="text"
         placeholder="Search here...."
-        className="h-[23px] w-full rounded-[8px] border-[0.59px] border-[#707175] bg-[#F6F6F6] pl-[10px] pr-6 text-[9px] font-normal text-[#858585] focus:outline-none tablet:h-[36px] tablet:pr-10 tablet:text-[16px] tablet:leading-[16px] dark:border-[#989898] dark:bg-[#000] dark:text-[#E8E8E8]"
+        className="h-[23px] small_text w-full rounded-[8px] border-[0.59px] border-[#707175] bg-[#F6F6F6] pl-[10px] pr-6 font-normal text-[#858585] focus:outline-none tablet:h-[36px] tablet:pr-10 tablet:text-[16px] tablet:leading-[16px] dark:border-[#989898] dark:bg-[#000] dark:text-[#E8E8E8]"
         // value={search}
         onChange={(e) => console.log(e.target.value)}
       />
